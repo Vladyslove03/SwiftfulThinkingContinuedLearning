b840522a5856c247395bb69e9f62b71dca6b7de8
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-08 10:08:28 +0300

    Initial Commit

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by User on 08.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
